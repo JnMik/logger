318ec75b42268f48e2edb83dd996ada422b0d9b3
jm <cyrjeanmichael@gmail.com>
2017-09-04 13:26:53 -0300

    AUtomation le prefix datetime sur le fwrite logger

--- src/fwriteLogger.php
+++ src/fwriteLogger.php
@@ -8,6 +8,7 @@
 
 namespace Support3w\Logger;
 
+use DateTime;
 
 class fwriteLogger implements Logger {
 
@@ -15,13 +16,22 @@ class fwriteLogger implements Logger {
      * @var
      */
     private $logFilepath;
+    /**
+     * @var bool
+     */
+    private $prefixLineWithDatetime;
 
-    public function __construct($logFilepath) {
+    public function __construct($logFilepath, $prefixLineWithDatetime = false) {
         $this->logFilepath = $logFilepath;
+        $this->prefixLineWithDatetime = $prefixLineWithDatetime;
     }
 
     public function log($message)
     {
+        if($this->prefixLineWithDatetime) {
+            file_put_contents($this->logFilepath, (new Datetime())->format('Y-m-d H:i:s') . " ", FILE_APPEND);
+        }
+
         if(is_array($message)) {
             file_put_contents($this->logFilepath, print_r($message, true) . "\n", FILE_APPEND);
         }else{
